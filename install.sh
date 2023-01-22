2fb05f91aa02c93411fbc4e1fb9928f2e81d6ca7
root <root@docker.proxmox>
2023-01-22 21:47:36 +0000

    install preparation for docker installation

--- install.sh
+++ install.sh
@@ -3,12 +3,13 @@
 ##########################
 #System preparation
 ##########################
-apt update && apt upgrade -y
-apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get -y install docker-ce docker-ce-cli containerd.io
+#apt update && apt upgrade -y
+#apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+#apt-get update
+#apt-get -y install docker-ce docker-ce-cli containerd.io
+
 ##########################
 #Navidrome and Podgrab
 ##########################
@@ -20,8 +21,8 @@ docker compose up -d
 #Spotdl
 ##########################
 #install python and spotdl
-apt install python3
-apt intall python3-pip
+apt install -y python3
+apt install -y python3-pip
 pip3 install spotdl
 spotdl --download-ffmpeg
 #link config file
